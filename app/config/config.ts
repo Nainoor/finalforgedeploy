f00aa615888e0e11b04ac68f5abdd747c5705884
Nainoor <nainoorpatel@gmail.com>
2018-12-25 17:06:24 +0530

    Create config.ts

---
+++ app/config/config.ts
@@ -0,0 +1,11 @@
+///<reference path='../typings/node/node.d.ts' />
+var config = {
+
+    consumerKey: process.env.FORGE_CLIENT_ID || '<your consumer key>',
+    consumerSecret: process.env.FORGE_CLIENT_SECRET || '<your consumer secret>'
+};
+
+export = config;
+
+
+
