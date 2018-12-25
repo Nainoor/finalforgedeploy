afedcf0ea43e7371d07452f32f4ece556e4a6ccc
Nainoor <nainoorpatel@gmail.com>
2018-12-25 17:10:03 +0530

    Add files via upload

---
+++ app/typings/form-data/form-data-tests.js
@@ -0,0 +1,9 @@
+/// <reference path="form-data.d.ts" />
+"use strict";
+var formData = require('form-data');
+var value;
+var fd = new formData.FormData();
+var obj = fd.getHeaders();
+value = fd.pipe(value);
+
+//# sourceMappingURL=form-data-tests.js.map
